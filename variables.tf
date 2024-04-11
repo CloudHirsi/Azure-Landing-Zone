7e3f5c25d4d8e8e4e31162f082bfa6d4fb754735
CloudHirsi <cloudhirsi@outlook.com>
2024-04-11 18:13:05 +0100

    first commit

---
+++ variables.tf
@@ -0,0 +1,28 @@
+variable "tenant_id" {
+
+}
+
+variable "object_id" {
+
+}
+
+variable "subscription_id" {
+
+}
+
+
+variable "vm_admin_username" {
+
+}
+
+variable "administrator_login" {
+
+}
+
+variable "administrator_login_password" {
+
+}
+
+variable "shared_key" {
+
+}
