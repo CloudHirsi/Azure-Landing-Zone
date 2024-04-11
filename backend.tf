c0b0ac8ae7e1b8b72fac16eaa90b0d07b4a9dbaf
CloudHirsi <cloudhirsi@outlook.com>
2024-04-11 20:03:12 +0100

    changed tfstate remote backend

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "Identity"
+    resource_group_name  = "tfstate"
     storage_account_name = "tfbackend7890"
     container_name       = "tfbackend"
     key                  = "prod.terraform.tfstate"
